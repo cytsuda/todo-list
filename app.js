e1207a8863c6d990aad70cf4fc7fe6b8f5f09e69
Carlos Yoshio Tsuda <cyt231@gmail.com>
2019-03-21 17:53:31 -0300

    Add gitignore and Procfile

--- app.js
+++ app.js
@@ -167,8 +167,12 @@ app.get("/:topic", function (req, res) {
 });
 
 // ==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>==<>
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 // Ativa o server e esperar acesso na porta:3000
-app.listen(3000, function () {
-  console.log("Server started on port 3000");
+app.listen(port, function () {
+  console.log("Server started has started successfully.");
 });
